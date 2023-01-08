e218262759d7eb3bad6b4189a9a2fa4ad1a95be8
NO-TORY <97148865+NO-TORY@users.noreply.github.com>
2023-01-08 23:00:33 +0900

    Create Second.c

---
+++ Formative-Evaluation/Second.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    double a = 80.5;
+    double b = 22.34;
+    double c = a + b;
+
+    printf("\t%.2f %.2f %.2f", a, b, c);
+    return 0;
+}
