9011af0f8df5e1dddc7c1549e9ed25f91ccaf9b0
NO-TORY <97148865+NO-TORY@users.noreply.github.com>
2023-01-14 13:44:26 +0900

    Update 64_65.c

--- selfcheck/64_65.c
+++ selfcheck/64_65.c
@@ -14,7 +14,7 @@ int fourth()
 int fifth()
 {
 	int a, b, c;
-	printf("세개의 정수를 입력하세요 (띄어쓰기로 구분): ");
+	printf("세 개의 정수를 입력하세요 (띄어쓰기로 구분): ");
 	scanf_s("%d %d %d", &a, &b, &c);
 	int r1 = (a == b);
 	int r2 = (b == c);
