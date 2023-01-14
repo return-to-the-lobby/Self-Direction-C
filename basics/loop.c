31ac485cf78c18ff7d8e9dc12bc687280ab4f3f9
NO-TORY <97148865+NO-TORY@users.noreply.github.com>
2023-01-14 14:15:51 +0900

    Update loop.c

--- basics/loop.c
+++ basics/loop.c
@@ -1,1 +1,29 @@
+#include <stdio.h>
+#include <stdlib.h>
 
+int for_loop()
+{
+	for (int i = 1; 5 > i - 1; i++) {
+		printf("%d\n", i);
+	}
+	return 0;
+}
+
+int while_loop()
+{
+	int i = 0;
+	while (1) {
+		if (i > 5) {
+			break;
+		}
+		printf("%d\n", i);
+		i += 1;
+	}
+	return 0;
+}
+
+int main() {
+	for_loop();
+	while_loop();
+	return 0;
+}
