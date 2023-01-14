714f45478379dbcfce8fd62db67a71e80b351bae
NO-TORY <97148865+NO-TORY@users.noreply.github.com>
2023-01-14 13:42:55 +0900

    Create 64_65.c

---
+++ selfcheck/64_65.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int fourth()
+{
+	int a, b, c;
+	printf("두 개의 정수를 입력하세요 (띄어쓰기로 구분): ");
+	scanf_s("%d %d", &a, &b);
+	c = a++ * --b;
+	printf("%d %d %d", a, b, c);
+	return 0;
+}
+
+int fifth()
+{
+	int a, b, c;
+	printf("세개의 정수를 입력하세요 (띄어쓰기로 구분): ");
+	scanf_s("%d %d %d", &a, &b, &c);
+	int r1 = (a == b);
+	int r2 = (b == c);
+	int r3 = (a != b);
+	int r4 = (b != c);
+
+	printf("%d %d %d %d", r1, r2, r3, r4);
+	return 0;
+}
+
+int main()
+{
+	// return fourth();
+	// return fifth();
+}
