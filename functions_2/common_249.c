060ee0279a343b7d41bcf7b1afc390021f6cd90e
로비로 돌아가 <97148865+return-to-the-lobby@users.noreply.github.com>
2023-03-08 17:56:01 +0900

    Create common_249.c

---
+++ functions_2/common_249.c
@@ -0,0 +1,25 @@
+#define _CRT_SECURE_NO_WARNINGS
+
+#include <stdio.h>
+#include <stdlib.h>
+
+#include <math.h>
+
+float get_area(float radius) {
+	return radius * radius * 3.14;
+}
+
+int main() {
+	float radius;
+	printf("Radius of a circle:");
+	scanf("%f", &radius);
+
+	float area = get_area(radius);
+
+	float area_floor = floorf(area);
+	float area_round = roundf(area);
+	float area_ceil = ceilf(area);
+
+	printf("area of the circle: %.0f\nfloored: %.0f\nrounded: %.0f\nceiled:%.0f\n", area, area_floor, area_round, area_ceil);
+	return 0;
+}
